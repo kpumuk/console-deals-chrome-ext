872662303597c88a7b0ab6b225cfd303cd468c25
j0be <ron.troyer@gmail.com>
2022-02-20 00:26:50 -0500

    Make new store layout work

--- src/content.js
+++ src/content.js
@@ -1,231 +1,279 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  switch(location.host) {
-    case "store.playstation.com":
-      return renderPlayStationTable(sendResponse, request.region, request.platform, request.start, request.size);
-    case "www.xbox.com":
-      return renderXboxTable(sendResponse);
-    default:
-      return sendResponse({error: "Unknown website, should never happen"});
-  }
+    switch (location.host) {
+        case "store.playstation.com":
+            return renderPlayStationTable(sendResponse, request.region, request.platform, request.start, request.size);
+        case "www.xbox.com":
+            return renderXboxTable(sendResponse);
+        default:
+            return sendResponse({
+                error: "Unknown website, should never happen"
+            });
+    }
 
-  function renderPlayStationTable(sendResponse, region, platform, start, size) {
-    var sale = parseSaleInfo(region);
+    function renderPlayStationTable(sendResponse, region, platform, start, size) {
+        let clearFilterButton = document.querySelector('[data-track-click="web:store:active-filters-clear-button"]');
+        clearFilterButton && clearFilterButton.click();
 
-    // Fetch sale details
-    var url = buildPlaystationStoreApiUrl(sale.locale, sale.country, sale.cid, platform, start, size);
-    $.getJSON(url)
-      .done((d) => {
-        if (d.included.length == 0) {
-          return sendResponse({error: "No results found, reloading in 5 seconds...", retryIn: 5000});
+        adjustFilters(region, platform);
+        loadDeal()
+            .then((dealObj) => {
+                let clearFilterButton = document.querySelector('[data-track-click="web:store:active-filters-clear-button"]');
+                clearFilterButton && clearFilterButton.click();
+
+                let games = Object.keys(dealObj)
+                    .sort((a, b) => a.localeCompare(b))
+                    .map((key) => dealObj[key]);
+
+                var hasDiscounts = games.some((item) => item.discount);
+                var hasPlusDiscounts = games.some((item) => item.plusDiscount);
+
+                var reddit = renderPlaystationRedditTable(games, hasDiscounts, hasPlusDiscounts);
+                var html = renderPlaystationHTMLTable(games, hasDiscounts, hasPlusDiscounts);
+
+                var total = games.length;
+
+                sendResponse({
+                    reddit,
+                    html,
+                    total,
+                    start,
+                    size
+                });
+            });
+
+        // Async sendResponse
+        return true;
+    }
+
+    function adjustFilters(region, platform) {
+        let filterButton = document.querySelector('button[data-qa="ems-sdk-grid-sort-filter-btn-mobile"]');
+        filterButton && filterButton.click();
+
+        checkFilters(platform);
+        checkSort();
+
+        filterButton = document.querySelector('button[data-qa="ems-sdk-grid-sort-filter-btn-mobile"]');
+        filterButton && filterButton.click();
+    }
+
+    function checkFilters(platform) {
+        let filterMapper = [
+            'storeDisplayClassification:FULL_GAME',
+            'storeDisplayClassification:GAME_BUNDLE',
+            'storeDisplayClassification:PREMIUM_EDITION'
+        ];
+        Array.from(document.querySelectorAll('.ems-sdk-filters__facet input')).forEach((node) => {
+            let filterName = node.parentElement.getAttribute('for');
+            let shouldCheck = filterMapper.includes(filterName);
+            let needsClick = (shouldCheck && !node.checked) || (!shouldCheck && node.checked);
+
+            if (needsClick) {
+                node.parentElement.click();
+            }
+        });
+
+        if (platform !== 'any') {
+            let platformToggle = document.querySelector('[data-qa="ems-sdk-collapsible-menu--targetPlatforms"]');
+            if (platformToggle && platformToggle.getAttribute('aria-expanded') !== 'true') {
+                platformToggle.click();
+            }
+
+            Array.from(document.querySelectorAll('[data-qa="ems-sdk-collabsable-menu-content--targetPlatforms"] button')).forEach((node) => {
+                let isPlatform = node.id === `targetPlatforms:${platform.toUpperCase()}`;
+                let isChecked = Array.from(node.classList).includes('checked');
+                let shouldClick = (isPlatform && !isChecked) || (!isPlatform && isChecked);
+
+                if (shouldClick) {
+                    node.click();
+                }
+            });
         }
-
-        var items = d.included.filter(isDiscountedPlaystationGame).
-          sort((a, b) => a.attributes.name.localeCompare(b.attributes.name));
-
-        var games = items.map((item) => parsePlaystationGames(sale.region, item));
-        var hasDiscounts = !!games.find((item) => item.discount);
-        var hasPlusDiscounts = !!games.find((item) => item.plusDiscount);
-
-        var reddit = renderPlaystationRedditTable(games, hasDiscounts, hasPlusDiscounts);
-        var html = renderPlaystationHTMLTable(games, hasDiscounts, hasPlusDiscounts);
-
-        var total = d.data.attributes['total-results'];
-
-        sendResponse({ reddit, html, total, start, size });
-      })
-      .fail(() => {
-        return sendResponse({ error: "Error returned. Maybe sale does not exist for the selected region?" });
-      });
-
-    // Async sendResponse
-    return true;
-  }
-
-  function parseSaleInfo(region) {
-    // Parse URL
-    var parsedUrl = parsePlaystationStoreUrl()
-
-    var cid = parsedUrl.cid;
-    if (cid === undefined) {
-      return sendResponse({ error: "Please navigate to a PlayStation sale page" });
     }
-    // Parse region
-    if (region === undefined || region === "") {
-      region = parsedUrl.region;
+
+    function checkSort() {
+        let sortButton = document.querySelector('.ems-sdk-collapsible-menu__sort');
+        if (sortButton) {
+            sortButton.click();
+            let radioButton = document.querySelector('input[value="productName:true"]');
+            radioButton && radioButton.click();
+        }
     }
-    var regionParts = region.split("-");
-    var locale = regionParts[0];
-    var country = regionParts[1];
 
-    return { cid, region, locale, country };
-  }
+    function loadDeal(resolver, dealObj) {
+        return new Promise((resolve) => {
+            resolver = resolver || resolve;
+            dealObj = dealObj || {};
+            waitForLoad()
+                .then(() => {
+                    parsePage(dealObj);
 
-  function htmlEscape(value) {
-    return $('<div/>').text(value).html();
-  }
-
-  function redditEscape(value) {
-    return value.replace("[", "\\[").replace("]", "\\]");
-  }
-
-  function buildPlaystationStoreApiUrl(locale, country, cid, platform, start, size) {
-    var url = [
-      'https://store.playstation.com/valkyrie-api',
-      locale,
-      country,
-      '19/container',
-      cid
-    ].join('/');
-    var params = {
-      sort:      'name',               // sort field
-      direction: 'asc',                // sort direction
-      platform:  platform,             // platform ID
-      start:     start,                // first record index
-      size:      size,                 // how many records to return
-      bucket:    'games',              // content type bucket
-      t:         new Date().getTime()  // cache buster
+                    let nextButton = document.querySelector('button[data-qa="ems-sdk-grid#ems-sdk-top-paginator-root#next"]');
+                    if (nextButton && !nextButton.disabled) {
+                        nextButton.click();
+                        loadDeal(resolver, dealObj);
+                    } else {
+                        resolver(dealObj);
+                    }
+                });
+        });
     }
-    return url + '?' + $.param(params);
-  }
 
-  function parsePlaystationStoreUrl() {
-    var matches = location.pathname.match(/.*\/(.*?)\/(grid|category)\/(.*?)\/.*/);
-    if (matches) {
-      return {
-        region: matches[1],
-        cid: matches[3],
-      }
+    function waitForLoad(resolver) {
+        return new Promise((resolve) => {
+            resolver = resolver || resolve;
+            if (document.querySelector('.psw-grid-list').textContent) {
+                resolver();
+            } else {
+                setTimeout(() => {
+                    waitForLoad(resolver);
+                }, 100);
+            }
+        });
     }
-  }
 
-  function parsePlaystationGames(region, item) {
-    var result = {
-      url:  'https://store.playstation.com/' + region + '/product/' + item.id,
-      name: item.attributes.name,
-    };
+    function parsePage(dealObj) {
+        Array.from(document.querySelectorAll('.psw-grid-list li')).forEach((node) => {
+            let name = node.querySelector('[data-qa*="#product-name"]')?.textContent.trim();
+            let platform = Array.from(node.querySelectorAll('[data-qa*="#game-art#tag"]'))?.map((platformNode) => {
+                return platformNode.textContent;
+            }).join('-');
 
-    var badge = item.attributes["badge-info"];
-    var itemHasNonPlusDiscount = badge["non-plus-user"] && !badge["non-plus-user"]["is-plus"];
-    var itemHasPlusDiscount =
-      badge["plus-user"] && (
-        !itemHasNonPlusDiscount ||
-        badge["plus-user"]["discount-percentage"] != badge["non-plus-user"]["discount-percentage"]
-      );
+            dealObj[`${name}-${platform}`] = dealObj[`${name}-${platform}`] ?? {
+                name: name,
+                platform: platform,
+                url: new URL(
+                        node.querySelector('[data-track="web:store:product-tile"]')?.getAttribute('href'),
+                        `${document.location.origin}${document.location.pathname.split('/').slice(0,-1).join('/')}`
+                    ).href,
+                originalPrice: node.querySelector('[data-qa*="#price#price-strikethrough"]')?.textContent
+            };
 
-    var defaultSku = item.attributes.skus.find((a) => a.id == item.attributes["default-sku-id"]);
-    var prices = defaultSku["prices"];
+            let product = dealObj[`${name}-${platform}`];
 
-    if (itemHasNonPlusDiscount) extractPlaystationPrice(prices, result, false);
-    if (itemHasPlusDiscount) extractPlaystationPrice(prices, result, true);
+            let discount = node.querySelector('[data-qa*="#discount-badge#text"]');
+            if (discount) {
+                let price = node.querySelector('[data-qa*="#price#display-price"]');
+                let isPlus = Array.from(discount.classList).includes('psw-discount-badge--ps-plus');
+                if (isPlus) {
+                    Object.assign(product, {
+                        plusPrice: price.textContent,
+                        plusDiscount: Math.abs(getNumber(discount.textContent)) + '%'
+                    });
+                } else {
+                    Object.assign(product, {
+                        price: price.textContent,
+                        discount: Math.abs(getNumber(discount.textContent)) + '%'
+                    });
+                }
+            }
 
-    return result;
-  }
+            let plusDiscount = node.querySelector('[data-qa*="#service-upsell"]');
+            if (plusDiscount) {
+                let plusDiscountNum = getNumber(plusDiscount.textContent) * -1
+                    + getNumber(product.discount);
 
-  function extractPlaystationPrice(prices, result, isPlus) {
-    var priceInfo = prices[isPlus ? "plus-user" : "non-plus-user"];
-    var price = priceInfo["actual-price"].display;
-    var discount = priceInfo["discount-percentage"];
-    discount = discount == 0 ? "–" : discount + "%";
+                Object.assign(product, {
+                    plusPrice: new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                        }).format(getNumber(product.price) * ((100 - Math.abs(plusDiscountNum))/100)),
+                    plusDiscount: `${plusDiscountNum}%`
+                });
+            }
+        });
+    }
 
-    result[(isPlus ? "plusPrice" : "price")] = price;
-    result[(isPlus ? "plusDiscount" : "discount")] = discount;
-  }
+    function getNumber(str) {
+        return Number(str.replace(/[^\d.]/g, ''));
+    }
 
-  function isDiscountedPlaystationGame(item) {
-    // Does it have a discount?
-    if (!item.attributes["badge-info"]) return false;
-    var badge = item.attributes["badge-info"];
-    if (!badge["non-plus-user"] || !badge["plus-user"]) return false;
-    // Does it have default SKU?
-    if (!item.attributes["default-sku-id"]) return false;
-    // Is it a game or game-related content?
-    if (!["game", "game-related"].includes(item.type)) return false;
-    // Is it a known non-game media?
-    if (["Dynamic Theme", "Theme", "Avatar", "Avatars", "Costume"].includes(item.attributes["game-content-type"])) return false;
-    if (item.attributes["game-content-type"] === "" && ["THEME"].includes(item.attributes["secondary-classification"])) return false;
 
-    // console.log({name: item.attributes.name, item});
+    function htmlEscape(value) {
+        return $('<div/>').text(value).html();
+    }
 
-    // Seems to be a game
-    return true;
-  }
+    function redditEscape(value) {
+        return value.replace("[", "\\[").replace("]", "\\]");
+    }
 
-  function renderPlaystationRedditTable(games, hasDiscounts, hasPlusDiscounts) {
-    var headers = [ "Game" ];
-    if (hasDiscounts) headers.push("Price", "% Off");
-    if (hasPlusDiscounts) headers.push("PS+", "% Off");
+    function renderPlaystationRedditTable(games, hasDiscounts, hasPlusDiscounts) {
+        var headers = ["Game"];
+        if (hasDiscounts) headers.push("Price", "% Off");
+        if (hasPlusDiscounts) headers.push("PS+", "% Off");
 
-    var result = redditTableRow(headers) + redditTableRow(headers.map(() => ":--"));
+        var result = redditTableRow(headers) + redditTableRow(headers.map(() => ":--"));
 
-    $(games).each((idx, game) => {
-      var cols = [ "[" + redditEscape(game.name) + "](" + game.url + ")" ];
+        $(games).each((idx, game) => {
+            var cols = ["[" + redditEscape(game.name) + "](" + game.url + ")"];
 
-      if (hasDiscounts) cols.push(game.price, game.discount);
-      if (hasPlusDiscounts) cols.push(game.plusPrice, game.plusDiscount);
+            if (hasDiscounts) cols.push(game.price, game.discount);
+            if (hasPlusDiscounts) cols.push(game.plusPrice, game.plusDiscount);
 
-      result += redditTableRow(cols);
-    });
-    return result;
-  }
+            result += redditTableRow(cols);
+        });
+        return result;
+    }
 
-  function renderPlaystationHTMLTable(games, hasDiscounts, hasPlusDiscounts) {
-    var headers = [ "Game (" + games.length + ")" ];
-    if (hasDiscounts) headers.push("Price", "% Off");
-    if (hasPlusDiscounts) headers.push("PS+", "% Off");
+    function renderPlaystationHTMLTable(games, hasDiscounts, hasPlusDiscounts) {
+        var headers = ["Game (" + games.length + ")"];
+        if (hasDiscounts) headers.push("Price", "% Off");
+        if (hasPlusDiscounts) headers.push("PS+", "% Off");
 
-    var result = "<thead>" + htmlTableRow(headers, "th") + "</thead><tbody>";
+        var result = "<thead>" + htmlTableRow(headers, "th") + "</thead><tbody>";
 
-    $(games).each((idx, game) => {
-      var cols = [ "<a href=\"" + game.url + "\">" + htmlEscape(game.name) + "</a>" ];
+        $(games).each((idx, game) => {
+            var cols = ["<a href=\"" + game.url + "\">" + htmlEscape(game.name) + "</a>"];
 
-      if (hasDiscounts) cols.push(game.price, game.discount);
-      if (hasPlusDiscounts) cols.push(game.plusPrice, game.plusDiscount);
+            if (hasDiscounts) cols.push(game.price, game.discount);
+            if (hasPlusDiscounts) cols.push(game.plusPrice, game.plusDiscount);
 
-      result += htmlTableRow(cols);
-    });
+            result += htmlTableRow(cols);
+        });
 
-    result += "</tbody></table>";
-    return result;
-  }
+        result += "</tbody></table>";
+        return result;
+    }
 
-  function htmlTableRow(cols, tag) {
-    if (tag === undefined) tag = "td";
-    return "<tr>" + cols.map((c) => "<" + tag + ">" + (c || "") + "</" + tag + ">").join() + "</tr>"
-  }
+    function htmlTableRow(cols, tag) {
+        if (tag === undefined) tag = "td";
+        return "<tr>" + cols.map((c) => "<" + tag + ">" + (c || "") + "</" + tag + ">").join() + "</tr>"
+    }
 
-  function redditTableRow(cols) {
-    return cols.map((c) => c || "").join("|") + "\n";
-  }
+    function redditTableRow(cols) {
+        return cols.map((c) => c || "").join("|") + "\n";
+    }
 
-  function renderXboxTable(sendResponse) {
-    var urlRegion = document.URL.split("/")[3].toLowerCase();
-    var countryCode = urlRegion.split("-")[1].toUpperCase();
+    function renderXboxTable(sendResponse) {
+        var urlRegion = document.URL.split("/")[3].toLowerCase();
+        var countryCode = urlRegion.split("-")[1].toUpperCase();
 
-    var rawGuids = []
-    $(".gameDiv[data-bigid]").each((_idx, el) => {
-      rawGuids.push($(el).data("bigid"));
-    });
-    console.log(rawGuids);
-    var guidUrl = 'https://displaycatalog.mp.microsoft.com/v7.0/products?bigIds=' + rawGuids.join(",") + '&market=' + countryCode + '&languages=' + urlRegion + '&MS-CV=DGU1mcuYo0WMMp+F.1';
+        var rawGuids = []
+        $(".gameDiv[data-bigid]").each((_idx, el) => {
+            rawGuids.push($(el).data("bigid"));
+        });
+        console.log(rawGuids);
+        var guidUrl = 'https://displaycatalog.mp.microsoft.com/v7.0/products?bigIds=' + rawGuids.join(",") + '&market=' + countryCode + '&languages=' + urlRegion + '&MS-CV=DGU1mcuYo0WMMp+F.1';
 
-    var result = "Game|Price|% Off\n|:--|:--|:--\n";
-    $.getJSON(guidUrl, (d) => {
-      $('.x1Games section.m-product-placement-item').each((idx, el) => {
-        var bigid = $(el).attr('data-bigid');
-        var product = d.Products.find((g) => g.ProductId == bigid);
-        // console.log(product.DisplaySkuAvailabilities[0].Availabilities);
+        var result = "Game|Price|% Off\n|:--|:--|:--\n";
+        $.getJSON(guidUrl, (d) => {
+            $('.x1Games section.m-product-placement-item').each((idx, el) => {
+                var bigid = $(el).attr('data-bigid');
+                var product = d.Products.find((g) => g.ProductId == bigid);
+                // console.log(product.DisplaySkuAvailabilities[0].Availabilities);
 
-        result +=
-          "[" + $('.x1GameName', el)[0].innerText.replace("[", "\\[").replace("]", "\\]") + "]" +
-          "(" + 'https://www.microsoft.com/en-ca/store/p/-/' + bigid + ")|" +
-          (product ? "$" + product.DisplaySkuAvailabilities[0].Availabilities[1].OrderManagementData.Price.ListPrice : "—") + "|" +
-          $('.x1GamePrice', el)[0].innerText +
-          "\n";
-      });
-      sendResponse({reddit: result});
-    });
+                result +=
+                    "[" + $('.x1GameName', el)[0].innerText.replace("[", "\\[").replace("]", "\\]") + "]" +
+                    "(" + 'https://www.microsoft.com/en-ca/store/p/-/' + bigid + ")|" +
+                    (product ? "$" + product.DisplaySkuAvailabilities[0].Availabilities[1].OrderManagementData.Price.ListPrice : "—") + "|" +
+                    $('.x1GamePrice', el)[0].innerText +
+                    "\n";
+            });
+            sendResponse({
+                reddit: result
+            });
+        });
 
-    // Async sendResponse
-    return true;
-  }
+        // Async sendResponse
+        return true;
+    }
 });
